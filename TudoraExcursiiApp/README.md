76df3a5199ea519706544628784a40c940b81a98
adrian <adrian@DESKTOP-4KUUVGC>
2019-06-08 05:57:37 +0300

    Angular + electron install

---
+++ TudoraExcursiiApp/README.md
@@ -0,0 +1,5 @@
+# Desktop app starter base with Angular 4 and Electron
+
+For further details - http://tphangout.com/?p=747
+
+
---
+++ TudoraExcursiiApp/main.js
@@ -0,0 +1,102 @@
+var { app, BrowserWindow } = require('electron');
+var url = require('url');
+var path = require('path');
+
+let win = null;
+
+if (handleSquirrelEvent()) {
+  // squirrel event handled and app will exit in 1000ms, so don't do anything else
+  return;
+}
+
+function handleSquirrelEvent() {
+  if (process.argv.length === 1) {
+    return false;
+  }
+
+  const ChildProcess = require('child_process');
+  const path = require('path');
+
+  const appFolder = path.resolve(process.execPath, '..');
+  const rootAtomFolder = path.resolve(appFolder, '..');
+  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+  const exeName = path.basename(process.execPath);
+
+  const spawn = function(command, args) {
+    let spawnedProcess, error;
+
+    try {
+      spawnedProcess = ChildProcess.spawn(command, args, {detached: true});
+    } catch (error) {}
+
+    return spawnedProcess;
+  };
+
+  const spawnUpdate = function(args) {
+    return spawn(updateDotExe, args);
+  };
+
+  const squirrelEvent = process.argv[1];
+  switch (squirrelEvent) {
+    case '--squirrel-install':
+    case '--squirrel-updated':
+      // Optionally do things such as:
+      // - Add your .exe to the PATH
+      // - Write to the registry for things like file associations and
+      //   explorer context menus
+
+      // Install desktop and start menu shortcuts
+      spawnUpdate(['--createShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-uninstall':
+      // Undo anything you did in the --squirrel-install and
+      // --squirrel-updated handlers
+
+      // Remove desktop and start menu shortcuts
+      spawnUpdate(['--removeShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-obsolete':
+      // This is called on the outgoing version of your app before
+      // we update to the new version - it's the opposite of
+      // --squirrel-updated
+
+      app.quit();
+      return true;
+  }
+};
+
+app.on('ready', () => {
+
+    win = new BrowserWindow({ width: 1000, height: 600 });
+     
+   // win.loadURL('http://localhost:4200');
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, 'src/index.html'),
+      protocol: 'file:',
+      slashes: true
+    }));
+    
+
+    win.on('closed', () => {
+        win = null;
+    })
+
+    win.webContents.openDevTools();
+})
+
+app.on('activate', () => {
+    if (win == null)
+    createWindow()    
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+})
